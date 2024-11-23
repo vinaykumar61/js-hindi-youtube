343aaac4fbee7123a343dd43ef9bc3f53a4e4542
Vinay Kumar <139650188+vinaykumar61@users.noreply.github.com>
2024-11-23 08:33:29 +0000

    comparison operation

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // null converted to 0
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// Avoid kare line no 8-18 tak ka code 
+// kyuki ye confusion wala dev m aisa code use nhi kiya jata 
+// null ko kabhi 0 m and kabhi NaN m convert karta hai 
+
+// == v/s ===
+console.log("2" == 2);    //true (only value comparison)
+console.log("2" === 2);   // false (data type and value comparison)
+
+
+// Same data types ka comparison sahi rahta hai
